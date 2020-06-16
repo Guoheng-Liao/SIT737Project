88e4377a8f32ff5ecefcc8bede6f161d0f230724
Guoheng-Liao <liaoguoheng36@gmail.com>
2020-06-16 17:36:49 +1000

    modify some problems

--- src/Components/Community.js
+++ src/Components/Community.js
@@ -56,20 +56,6 @@ export default class Comunity extends Component {
                     </tbody>
                 </table>
             </div>
-            <div class="footer">
-                    <footer class="footer-distributed">
-                        <div class="footer-right">
-                            <a href="http://www.facebook.com/sharer/sharer.php?u=&lt;URL&gt;"><i class="fa fa-facebook"></i></a>
-
-                            <a href="http://twitter.com/share?url=&lt;URL&gt;&amp;text=&lt;TEXT&gt;&amp;via=&lt;VIA&gt;"><i class="fa fa-twitter"></i></a>
-                            <a href="mailto:?subject=&lt;SUBJECT&amp;body=&lt;BODY&gt;"><i class="fa fa-mail-forward"></i></a></div>
-                        <div class="footer-left">
-                            <p class="footer-links"><a href="#">Back to Top</a></p>
-                            <p>Game Player Zone © 2020</p>
-                        </div>
-                    </footer>
-                </div>
-
             </React.Fragment>
         );
     }
